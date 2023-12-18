a39c8b248afa8dc88581082066736155efb0ad90
gogicha007 <gogicha@gmail.com>
2023-12-18 18:18:49 +0400

    chor: just save the work

--- test.js
+++ test.js
@@ -1,6 +1,35 @@
-function calculateHanoi(disksNumber, turnsSpeed) {
-  const result = Math.pow(2, disksNumber) - 1;
-  return { turns: result, seconds: Math.floor(result / (turnsSpeed / 3600)) };
+function getMatrixElementsSum(matrix) {
+  const height = matrix.length;
+  const width = matrix[0].length;
+  let rows = [];
+  let columns = [];
+  for (let c = 0; c < width; c++) {
+    for (let r = 0; r < height; r++) {
+      rows.push(matrix[r][c]);
+    }
+    columns.push(rows);
+    rows = [];
+  }
+  const result = columns.reduce((acc, val) => {
+    return acc +
+      val.reduce((subAcc, subVal, idx, arr) => {
+        if (idx === 0) {
+          subAcc += subVal;
+        } else if (arr[idx - 1] !== 0) {
+          subAcc += subVal;
+        }
+        return subAcc;
+      }, 0);
+  }, 0);
+  return result;
+  // console.log(`${rows} - ${columns}`);
+
+  // return matrix.flat().reduce((acc, val) => acc + val, 0);
 }
 
-console.log(calculateHanoi(5, 4074));
+matrix = [
+  [0, 1, 1, 2],
+  [0, 5, 0, 0],
+  [2, 0, 3, 3],
+];
+console.log(getMatrixElementsSum(matrix));
